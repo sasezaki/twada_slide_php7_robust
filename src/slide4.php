bf70396693b2de2c0ed7fcd750b22cc0c6f5d835
sasezaki <sasezaki@gmail.com>
2019-08-31 18:21:09 +0900

    PSR2に基づき、複数行にかかわるコンストラクタの呼び出しをやめる。空白のとりやめ
    ---------------------------------------------------------------------------------------------------------
    
    FOUND 5 ERRORS AFFECTING 2 LINES
    
    ---------------------------------------------------------------------------------------------------------
    
     13 | ERROR | [x] Opening parenthesis of a multi-line function call must be the last content on the line
    
     13 | ERROR | [x] Only one argument is allowed per line in a multi-line function call
    
     13 | ERROR | [x] Only one argument is allowed per line in a multi-line function call
    
     14 | ERROR | [x] Multi-line function call not indented correctly; expected 8 spaces but found 23
    
     14 | ERROR | [x] Closing parenthesis of a multi-line function call must be on a line by itself

--- src/slide4.php
+++ src/slide4.php
@@ -10,8 +10,7 @@ class BugRepository
     public static function findAll($params)
     {
         global $CONF;
-        $pdo = new PDO($CONF['dsn'], $CONF['usr'], $CONF['passwd'],
-                       [ PDO::ATTR_EMULATE_PREPARES => false]);
+        $pdo = new PDO($CONF['dsn'], $CONF['usr'], $CONF['passwd'], [ PDO::ATTR_EMULATE_PREPARES => false]);
         $sql = 'SELECT bug_id, summary, date_reported FROM Bugs
             WHERE assigned_to = :assignedTo AND status = :status';
         $stmt = $pdo->prepare($sql);
